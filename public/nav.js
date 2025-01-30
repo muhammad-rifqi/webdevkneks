d8028050a0173d9253c91f5b5dbb04cabe9ff475
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-01-30 20:00:06 +0700

    udpate

--- public/nav.js
+++ public/nav.js
@@ -36,7 +36,7 @@ if (cookie["roles_id"] == 1 || cookie["roles_id"] == 2) {
                     </li>`);
 }
 //Direktorat
-if (cookie["roles_id"] == 1 || cookie["roles_id"] == 2) {
+if (cookie["roles_id"] == 1 || cookie["roles_id"] == 2 || cookie["roles_id"] == 3 || cookie["roles_id"] == 4) {
     document.write(`<li class="nxl-item nxl-hasmenu">
                         <a href="javascript:void(0);" class="nxl-link">
                             <span class="nxl-micon"><i class="feather-bookmark"></i></span><span class="nxl-arrow"><i class="feather-chevron-right"></i></span>
@@ -49,16 +49,8 @@ if (cookie["roles_id"] == 1 || cookie["roles_id"] == 2) {
                     </li>`);
 }
 
-//Direktorat
-if (cookie["roles_id"] == 3 || cookie["roles_id"] == 4) {
-    document.write(`<li class="nxl-item nxl-hasmenu">
-                        <a href="/directorats" class="nxl-link">
-                            <span class="nxl-micon"><i class="feather-bookmark"></i></span><span class="nxl-arrow">Direktorat</span>
-                            Direktorat Master
-                        </a>
-                    </li>`);
 
-    }
+
 //Struktur
 if (cookie["roles_id"] == 1 || cookie["roles_id"] == 2) {
     document.write(`<li class="nxl-item nxl-hasmenu">
@@ -86,17 +78,7 @@ if (cookie["roles_id"] == 3 || cookie["roles_id"] == 4) {
 }
 
 //Berita
-if(cookie["roles_id"] == 3 || cookie["roles_id"] == 4 || cookie["roles_id"] == 6){
-    document.write(` <li class="nxl-item nxl-hasmenu">
-                        <a href="/n" class="nxl-link">
-                            <span class="nxl-micon"><i class="feather-bookmark"></i></span>
-                            <span class="nxl-mtext">Berita</span>
-                        </a>
-                    </li>
-        `);
-}
-
-if (cookie["roles_id"] == 1 || cookie["roles_id"] == 2) {
+if (cookie["roles_id"] == 1 || cookie["roles_id"] == 2 || cookie["roles_id"] == 3 || cookie["roles_id"] == 4 || cookie["roles_id"] == 6) {
     document.write(` <li class="nxl-item nxl-hasmenu">
                         <a href="javascript:void(0);" class="nxl-link">
                             <span class="nxl-micon"><i class="feather-bookmark"></i></span>
@@ -190,7 +172,7 @@ if (cookie["roles_id"] == 1 || cookie["roles_id"] == 2 || cookie["roles_id"] == 
                             <span class="nxl-mtext">E-Pustaka</span><span class="nxl-arrow"><i class="feather-chevron-right"></i></span>
                         </a>
                         <ul class="nxl-submenu">
-                            <li class="nxl-item"><a class="nxl-link" href="/f">e-Pustaka</a></li>
+                            <li class="nxl-item"><a class="nxl-link" href="/f">E-Pustaka</a></li>
                             `);
 }
 //e-pustaka kategori
