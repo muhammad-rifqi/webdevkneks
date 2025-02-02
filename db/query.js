6b3235878feac4e4ed19ed484b217fa95412c33b
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-02-02 16:21:22 +0700

    udpate

--- db/query.js
+++ db/query.js
@@ -2913,7 +2913,7 @@ const dropdown_menu = async (req, res) => {
                 "menu_name": item?.menu_name,
                 "menu_link": item?.menu_link,
                 "menu_orders": item?.orders,
-                "menu_name": item?.menu_name,
+                "menu_name_en": item?.menu_name_en,
                 "menu_sub": sub_menux
             };
             resolve(row);
