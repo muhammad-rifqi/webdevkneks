c4a1e9cf1adc2e178e7ec1d9e4fdb86febb78133
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-02-12 13:53:53 +0700

    update

--- public/nav.js
+++ public/nav.js
@@ -233,7 +233,7 @@ if (cookie["roles_id"] == 1 || cookie["roles_id"] == 2 || cookie["roles_id"] == 
 }
 
 if (cookie["roles_id"] == 3 || cookie["roles_id"] == 4 || cookie["roles_id"] == 6) {
-    document.write(`li class="nxl-item nxl-hasmenu">
+    document.write(`<li class="nxl-item nxl-hasmenu">
                         <a href="/master" class="nxl-link">
                             <span class="nxl-micon"><i class="feather-bookmark"></i></span>
                             KDEKS
