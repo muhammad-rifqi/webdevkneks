dcbe0e001cf8e681ad585a6279bed373f6ef9689
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-02-17 22:22:46 +0700

    update

--- public/nav.js
+++ public/nav.js
@@ -181,14 +181,10 @@ if (cookie["roles_id"] == 1 || cookie["roles_id"] == 2) {
 
 if(cookie["roles_id"] == 3 || cookie["roles_id"] == 4 || cookie["roles_id"] == 6){
     document.write(` <li class="nxl-item nxl-hasmenu">
-                        <a href="javascript:void(0);" class="nxl-link">
+                        <a href="/f" class="nxl-link">
                             <span class="nxl-micon"><i class="feather-bookmark"></i></span>
-                            <span class="nxl-mtext">e-Pustaka</span><span class="nxl-arrow"><i class="feather-chevron-right"></i></span>
+                            e-Pustaka
                         </a>
-                        <ul class="nxl-submenu">
-                            <li class="nxl-item"><a class="nxl-link" href="/f">e-Pustaka</a></li>
-                            <li class="nxl-item"><a class="nxl-link" href="/fc">Kategori</a></li>
-                        </ul>
                     </li>`);
 
 }
