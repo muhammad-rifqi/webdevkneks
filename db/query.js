59e74120d6e00dcbef0e2389699eafd48932393d
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-06-12 20:05:07 +0700

    update

--- db/query.js
+++ db/query.js
@@ -3629,18 +3629,18 @@ const submenu_detail = async (req, res) => {
 
 const download_image_base64 = async (req, res) => {
     const urls = req.body.puppeteer;
-    const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/chromium-browser', 
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
+    // const browser = await puppeteer.launch({
+    //     executablePath: '/usr/bin/chromium-browser', 
+    //     headless: true,
+    //     args: ['--no-sandbox', '--disable-setuid-sandbox']
+    // });
     // const page = await browser.newPage();
     // await page.setViewport({ width: 1280, height: 768 });
     // await page.goto(urls, { waitUntil: 'networkidle2', timeout: 60000 });
     // const screenshot = await page.screenshot({ fullPage: false, encoding: 'base64' });
     // await browser.close();
     // res.status(200).json({ "ss": `${screenshot}` })
-    res.status(200).json({"version":puppeteer.version, "url" : urls, "addon" : browser});
+    res.status(200).json({"version":puppeteer.version, "url" : urls});
 }
 
 //::::::::::::::::::::::::::::::Start Of Modules:::::::::::::::::::::::::::::::::::::::::::::::::::::
