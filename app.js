d83487782946a0ff5b1bb8f38753aa630a89df53
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-09-23 16:39:01 +0700

    update

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ apps.use(
     })
 )
 
-const allowedOrigins = ["https://cms-demo.kneks.go.id/","https://cms-demo.kneks.go.id","https://fonts.gstatic.com/","https://fonts.googleapis.com/"];
+const allowedOrigins = ["https://cms-demo.kneks.go.id/","https://cms-demo.kneks.go.id"];
 apps.use(cors({
     origin: function (origin, callback) {
         if (!origin || allowedOrigins.includes(origin)) {
