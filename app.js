468a32e36a05b2064ee10cf3ed8c1cb0bf76b38f
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-09-24 10:38:49 +0700

    update

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ apps.options("*",cors());
 apps.use((req, res, next) => {
     res.setHeader('X-Frame-Options', 'DENY'); 
     res.setHeader('X-Content-Type-Options', 'nosniff'); 
-    res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' https://trusted.com");
+    // res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' https://trusted.com");
     res.setHeader('X-XSS-Protection', '1; mode=block'); 
     next();
 });
