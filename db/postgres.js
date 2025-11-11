c33e9cd3970929b10b43761b9663e6510a65b802
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-11-11 13:06:03 +0700

    udpate

--- db/postgres.js
+++ db/postgres.js
@@ -1,10 +1,10 @@
 const util = require('util');
 const pgsql = require('pg');
 const pool = new pgsql.Pool({
-    user: 'postgres',
+    user: 'kneks',
     host: 'localhost',
-    database: 'kneks_new',
-    password: '',
+    database: 'kneks',
+    password: 'Barlok15@',
     port: 5432,
 })
 
